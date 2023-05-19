554b99c8150d14983e9b4de7ba404c70f07fc5d3
onurae <oakin.ae@gmail.com>
2023-05-19 11:37:47 +0300

    Update main.cpp

--- src/main.cpp
+++ src/main.cpp
@@ -1,18 +1,83 @@
 #include <Arduino.h>
+#include "toneAC.h"
+#include "ST7567_FB.h"
 
-// put function declarations here:
-int myFunction(int, int);
+// Buttons
+#define BUTTON_OK PIN_A0
+#define BUTTON_BACK PIN_A1
+#define BUTTON_UP PIN_A2
+#define BUTTON_DOWN PIN_A3
 
-void setup() {
-  // put your setup code here, to run once:
-  int result = myFunction(2, 3);
+// LCD
+ST7567_FB lcd(7, 8, 6);
+
+// Main loop
+unsigned long start;
+unsigned long end;
+int8_t elapsed;
+
+void Delay(unsigned long interval)
+{
+    unsigned long previous = millis();
+    unsigned long current = previous;
+    while (current - previous < interval)
+    {
+        current = millis();
+    }
 }
 
-void loop() {
-  // put your main code here, to run repeatedly:
+void setup()
+{
+    Delay(500);
+    Serial.begin(115200);
+    Serial.println("vario-black");
+    toneAC(1000, 10, 200, true);
+
+    // Buttons
+    pinMode(BUTTON_OK, INPUT_PULLUP);
+    pinMode(BUTTON_BACK, INPUT_PULLUP);
+    pinMode(BUTTON_UP, INPUT_PULLUP);
+    pinMode(BUTTON_DOWN, INPUT_PULLUP);
+
+    // LCD
+    lcd.init();
+    lcd.setContrast(0);
+    lcd.cls();
+    lcd.display();
+
+    // Main loop
+    start = millis();
+
+    lcd.cls();
+    lcd.drawRectD(0, 0, SCR_WD, SCR_HT, 1);
+    int x = 16, y = 10;
+    lcd.drawRect(x + 8, y - 5, 20, 20, 1);
+    lcd.fillRect(x + 8 + 30, y - 5, 20, 20, 1);
+    lcd.fillRectD(x + 8 + 60, y - 5, 20, 20, 1);
+    lcd.drawCircle(x + 5 + 12, 48, y, 1);
+    lcd.fillCircle(x + 5 + 30 + 12, y + 36, 12, 1);
+    lcd.fillCircleD(x + 5 + 60 + 12, y + 36, 12, 1);
+    lcd.display();
 }
 
-// put function definitions here:
-int myFunction(int x, int y) {
-  return x + y;
+void loop()
+{
+    // Period
+    end = millis();
+    elapsed = end - start;
+    Serial.println(elapsed);
+    int8_t period = 100;
+    if (elapsed < period)
+    {
+        Delay(period - elapsed);
+    }
+    start = millis();
+
+    // Power off
+    if (digitalRead(BUTTON_BACK) == 0)
+    {
+        toneAC(1000, 10, 200, true);
+        Delay(500);
+        pinMode(BUTTON_OK, INPUT);
+    }
 }
