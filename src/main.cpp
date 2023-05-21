c859814415a21eaf8e058b70e20f1dd9168fe9d4
onurae <oakin.ae@gmail.com>
2023-05-21 18:16:40 +0300

    Update main.cpp

--- src/main.cpp
+++ src/main.cpp
@@ -10,6 +10,7 @@
 #include "ST7567_FB.h"
 #include "toneAC.h"
 #include "baro.hpp"
+#include "c64enh_font.h"
 
 // Buttons
 #define BUTTON_OK PIN_A0
@@ -17,16 +18,21 @@
 #define BUTTON_UP PIN_A2
 #define BUTTON_DOWN PIN_A3
 
-// LCD
-ST7567_FB lcd(7, 8, 6);
+// Screen
+ST7567_FB lcd(7, 8, 6); // dc, rst, cs.
+const int8_t fps = 2;   // 2Hz
+int8_t counterLcd = 0;
+int8_t iRefresh = 0;
+int8_t i = 0;
+void UpdateLcd();
 
 // Sensor
 Baro baro;
-const float dt = 0.1; // 10Hz
-int8_t counter = 0;
+const int8_t freq = 10; // 10Hz
+int8_t counterBaro = 0;
 
 // Main loop
-const int8_t period = 50; // Main loop period [ms]
+const int8_t period = 50; // [ms]
 unsigned long start;
 unsigned long end;
 int8_t elapsed;
@@ -43,35 +49,21 @@ void setup()
     pinMode(BUTTON_UP, INPUT_PULLUP);
     pinMode(BUTTON_DOWN, INPUT_PULLUP);
 
-    // LCD
+    // Screen
     lcd.init();
     lcd.setContrast(0);
+    lcd.setRotation(4);
     lcd.cls();
-    lcd.display();
 
-    if (baro.Init() == false) // Pressure refresh rate: (freq / 2). Max 50Hz when the main loop freq is 100Hz and above.
+    // Sensor [Pressure rate: (freq / 2). Max 50Hz when the main loop freq is 100Hz and above]
+    if (baro.Init() == false)
     {
         Serial.println(F("Sensor error"));
-        while (true)
-        {
-            Delay(1000);
-        }
+        LoopForever();
     }
 
     // Main loop
     start = millis();
-
-    // delete this.
-    lcd.cls();
-    lcd.drawRectD(0, 0, SCR_WD, SCR_HT, 1);
-    int x = 16, y = 10;
-    lcd.drawRect(x + 8, y - 5, 20, 20, 1);
-    lcd.fillRect(x + 8 + 30, y - 5, 20, 20, 1);
-    lcd.fillRectD(x + 8 + 60, y - 5, 20, 20, 1);
-    lcd.drawCircle(x + 5 + 12, 48, y, 1);
-    lcd.fillCircle(x + 5 + 30 + 12, y + 36, 12, 1);
-    lcd.fillCircleD(x + 5 + 60 + 12, y + 36, 12, 1);
-    lcd.display();
 }
 
 void loop()
@@ -87,14 +79,14 @@ void loop()
     start = millis();
 
     // Sensor
-    counter += 1;
-    if (counter >= (int8_t)(dt * 1000) / period)
+    counterBaro += 1;
+    if (counterBaro >= (1000 / freq / period))
     {
-        counter = 0;
-        baro.Update(dt);
+        counterBaro = 0;
+        baro.Update(1.0f / freq);
         if (baro.CheckFailure() == false)
         {
-            // Refresh the screen only when the state is true.
+            // Print baro values only when the state is true.
             if (baro.GetState() == true)
             {
                 Serial.print(baro.GetAlt());
@@ -102,6 +94,9 @@ void loop()
                 Serial.print(baro.GetX());
                 Serial.print(F("   "));
                 Serial.println(baro.GetV());
+                // Serial.print(F("   "));
+                // CalculateAverageVario(baro.GetV());
+                // Serial.println(vsAverage);
             }
         }
         else
@@ -110,11 +105,54 @@ void loop()
         }
     }
 
+    // Screen
+    counterLcd += 1;
+    if (counterLcd >= (1000 / fps / period))
+    {
+        counterLcd = 0;
+        UpdateLcd();
+    }
+
     // Power off
-    if (digitalRead(BUTTON_BACK) == 0)
+    if (IsButtonPressed(BUTTON_BACK) == true)
     {
         toneAC(1000, 10, 200, true);
         Delay(500);
         pinMode(BUTTON_OK, INPUT);
     }
+}
+
+void UpdateLcd()
+{
+    lcd.cls();
+    lcd.setFont(c64enh);
+    i += 1;
+    if (i > 100)
+    {
+        i = 0;
+    }
+    lcd.printStr(i + 5, 5, "X");
+
+    lcd.printStr(ALIGN_CENTER, 50, "Hello World!");
+    char buf[10];
+    int t1 = baro.GetX();
+    int t10 = baro.GetX() * 10 - t1 * 10;
+    snprintf(buf, 10, "%d.%d ", t1, t10);
+    lcd.setFont(Bold13x20);
+    lcd.printStr(20, 10, buf);
+    if (baro.GetV() < 0)
+    {
+        snprintf(buf, 10, "%s%d.%d ", "-", int8_t(baro.GetV() * -1), int8_t(baro.GetV() * -10) % 10);
+    }
+    else
+    {
+        snprintf(buf, 10, "%s%d.%d ", " ", int8_t(baro.GetV() * 1), int8_t(baro.GetV() * 10) % 10);
+    }
+
+    // lcd.printStr(80, 10, buf);
+    // lcd.drawRectD(0, 0, 128, 64, 1);
+    // lcd.drawRect(18, 20, 127 - 18 * 2, 63 - 20 * 2, 1);
+    
+    lcd.display();
+    // lcd.displayInvert(true);
 }
